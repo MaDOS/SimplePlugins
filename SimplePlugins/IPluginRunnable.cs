4f99c39e674548ae663d5b54f0bf9653ba002d11
BeR <r.bechtel@kuettner-automation.de>
2019-07-09 11:31:44 +0200

    1st prototype done

---
+++ SimplePlugins/IPluginRunnable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePlugins
+{
+  public interface IPluginRunnable
+  {
+    void Initialize();
+
+    void Start();
+
+    void Stop();
+  }
+}
---
+++ SimplePlugins/PluginManager.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePlugins
+{
+  public class PluginManager
+  {
+    private Dictionary<Type, IPluginRunnable> Plugins { get; set; }
+
+    public PluginManager()
+    {
+      this.Plugins = new Dictionary<Type, IPluginRunnable>();
+    }
+
+    public T GetPlugin<T>() where T : class, IPluginRunnable, new()
+    {
+      if (!this.Plugins.ContainsKey(typeof(T)))
+      {
+        return null;
+      }
+
+      return (T)this.Plugins[typeof(T)];
+    }
+
+    public void RegisterPlugin<T>() where T : class, IPluginRunnable, new()
+    {
+      if (this.Plugins.ContainsKey(typeof(T)))
+      {
+        return;
+      }
+
+      this.Plugins.Add(typeof(T), new T());
+    }
+
+    public void InitializePlugins()
+    {
+      foreach(IPluginRunnable plugin in this.Plugins.Values)
+      {
+        plugin.Initialize();
+      }
+    }
+
+    public void StartPlugins()
+    {
+      foreach(IPluginRunnable plugin in this.Plugins.Values)
+      {
+        plugin.Start();
+      }
+    }
+
+    public void StopPlugins()
+    {
+      foreach (IPluginRunnable plugin in this.Plugins.Values)
+      {
+        plugin.Stop();
+      }
+    }
+  }
+}
